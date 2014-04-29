122d3c23c4083b01402b3074d45f2c62dea6e9a2
Matt Cloyd <cloydster@gmail.com>
2014-04-29 15:28:19 -0400

    Fix Region relation, reorder abbreviations in fixtures

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,6 +1,8 @@
 class Region < ActiveRecord::Base
   attr_accessible :abbr, :name, :rpa_name
-  has_many :municipalities
+  
+  has_and_belongs_to_many :municipalities
+
 
   validates :name,     presence: true, length: { minimum: 3, maximum: 70 }
   validates :abbr,     presence: true, length: { minimum: 3, maximum: 10 }
--- db/fixtures/regions.rb
+++ db/fixtures/regions.rb
@@ -1,15 +1,15 @@
 def regions
-  [{ id: 1,  abbr: 'BRPC',   name: 'Berkshires',                 rpa_name: "Berkshire County Regional Planning Commission" },
-   { id: 2,  abbr: 'FRCOG',  name: 'Cape Cod',                   rpa_name: "Cape Cod Planning & Economic Development Commission" },
-   { id: 3,  abbr: 'PVPC',   name: 'Central Massachusetts',      rpa_name: "Central Massachusetts Planning Commission" },
-   { id: 4,  abbr: 'MRPC',   name: 'Franklin County',            rpa_name: "Franklin County Commission" },
-   { id: 5,  abbr: 'CMRPC',  name: "Martha's Vineyard",          rpa_name: "Martha's Vineyard Commission" },
-   { id: 6,  abbr: 'NMCOG',  name: 'Merrimac Valley',            rpa_name: "Merrimac Valley Planning Commission" },
-   { id: 7,  abbr: 'MVPC',   name: 'Metro Boston',               rpa_name: "Metropolitan Area Planning Council" },
-   { id: 8,  abbr: 'MAPC',   name: 'Montachusett',               rpa_name: "Montachusett Regional Planning Commission" },
-   { id: 9,  abbr: 'OCPC',   name: 'Nantucket',                  rpa_name: "Nantucket Planning & Economic Development Commission" },
-   { id: 10, abbr: 'SRPEDD', name: 'Northern Middlesex',         rpa_name: "Northern Middlesex Area Council of Government" },
-   { id: 11, abbr: 'CCC',    name: 'Old Colony',                 rpa_name: "Old Colony Planning Council" },
-   { id: 12, abbr: 'MVC',    name: 'Pioneer Valley',             rpa_name: "Pioneer Valley Planning Commission" },
-   { id: 13, abbr: 'NPEDC',  name: 'Southeastern Massachusetts', rpa_name: "Southeastern Regional Planning & Economic Development District" }]
+  [{ id: 1,  abbr: 'BRPC',    name: 'Berkshires',                 rpa_name: "Berkshire County Regional Planning Commission" },
+   { id: 2,  abbr: 'CCC',     name: 'Cape Cod',                   rpa_name: "Cape Cod Planning & Economic Development Commission" },
+   { id: 3,  abbr: 'CMRPC',   name: 'Central Massachusetts',      rpa_name: "Central Massachusetts Planning Commission" },
+   { id: 4,  abbr: 'FRCOG',   name: 'Franklin County',            rpa_name: "Franklin County Commission" },
+   { id: 5,  abbr: 'MVC',     name: "Martha's Vineyard",          rpa_name: "Martha's Vineyard Commission" },
+   { id: 6,  abbr: 'MVPC',    name: 'Merrimac Valley',            rpa_name: "Merrimac Valley Planning Commission" },
+   { id: 7,  abbr: 'MAPC',    name: 'Metro Boston',               rpa_name: "Metropolitan Area Planning Council" },
+   { id: 8,  abbr: 'MRPC',    name: 'Montachusett',               rpa_name: "Montachusett Regional Planning Commission" },
+   { id: 9,  abbr: 'NPEDC',   name: 'Nantucket',                  rpa_name: "Nantucket Planning & Economic Development Commission" },
+   { id: 10, abbr: 'NMCOG',   name: 'Northern Middlesex',         rpa_name: "Northern Middlesex Area Council of Government" },
+   { id: 11, abbr: 'OCPC',    name: 'Old Colony',                 rpa_name: "Old Colony Planning Council" },
+   { id: 12, abbr: 'PVPC',    name: 'Pioneer Valley',             rpa_name: "Pioneer Valley Planning Commission" },
+   { id: 13, abbr: 'SRPEDD',  name: 'Southeastern Massachusetts', rpa_name: "Southeastern Regional Planning & Economic Development District" }]
 end
